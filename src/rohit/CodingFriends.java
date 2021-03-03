552973780f3c073951ff9694390663a457df06cb
Rohit Kalia <kalia.rohit@yahoo.com>
2021-03-03 16:13:44 -0500

    Hacker code

---
+++ src/rohit/CodingFriends.java
@@ -0,0 +1,20 @@
+package rohit;
+
+public class CodingFriends {
+
+        public static int minNum(int samDaily, int kellyDaily, int difference)
+        {
+            // No catching up because samDaily is ahead (positive P) and kellyDaily cannot catch up.
+            if (kellyDaily <= samDaily)
+            {
+                return -1;
+            }
+            return difference/(kellyDaily - samDaily) + 1;
+        }
+
+    public static void main(String[] args) {
+        System.out.println(minNum(3,6,5));
+
+    }
+
+}
--- src/rohit/PracticeWork.java
+++ src/rohit/PracticeWork.java
@@ -1,35 +1,41 @@
 package rohit;
 
-import java.io.*;
-import java.util.*;
-import java.text.*;
-import java.math.*;
-import java.util.regex.*;
-
 public class PracticeWork {
 
-    private static final Scanner scan = new Scanner(System.in);
-
-        public static void main (String[]args) {
-            Scanner in = new Scanner(System.in);
-            int testCases = Integer.parseInt(in.nextLine());
-            while (testCases > 0) {
-                String line = in.nextLine();
-                boolean matchFound = false;
-                String regex = "<(.+)>([^<]+)</\\1>";
-                Pattern pat = Pattern.compile(regex);
-                Matcher match = pat.matcher(line);
-                while (match.find()) {
-                    System.out.println(match.group(2));
-                    matchFound = true;
-                }
-                if ( ! matchFound) {
-                    System.out.println("None");
-                }
-
-                testCases--;
+    private static int movesToMakeZigzag(int[] nums) {
+        if (nums == null || nums.length <= 1) return 0;
+        int odd = 0;
+        int even = 0;
+        // we want odd bigger, just dec even  9 6 1 6 2
+        for (int i = 0; i < nums.length; i+=2) {
+            if (i == 0) {
+                odd += Math.max(0, nums[i] - nums[i+1] + 1);
+            } else if (i == nums.length - 1) {
+                odd += Math.max(0, nums[i] - nums[i-1] + 1);
+            } else {
+                odd += Math.max(0, nums[i] - nums[i-1] + 1);
+                int tmp = nums[i] - Math.max(0, nums[i] - nums[i-1] + 1); // when u decrese for the left, it also contribute to the right;
+                odd += Math.max(0, tmp - nums[i+1] + 1);
             }
         }
+        // we want even bigger, just dec odd
+        for (int i = 1; i < nums.length; i+=2) {
+            if (i == nums.length - 1) {
+                even += Math.max(0, nums[i] - nums[i-1] + 1);
+            } else {
+                even += Math.max(0, nums[i] - nums[i-1] + 1);
+                int tmp = nums[i] - Math.max(0, nums[i] - nums[i-1] + 1);
+                even += Math.max(0, tmp - nums[i+1] + 1);
+            }
+        }
+        return odd < even ? odd : even;
+    }
+
+    public static void main(String[] args) {
+        int[] num = {1,2,3,4,7,9,4,5};
+        System.out.println(movesToMakeZigzag(num));
+    }
 
 }
+
 
